ce9e11c7bc80dd3d6a8e9a77677dbd9781d53f13
Josiah Salcido <commander225@gmail.com>
2019-04-17 15:43:29 -0700

    fix install script break

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-# Setup yay and install jsalix-base
+# Setup new machine (possibly from within chroot?)
 
-# TODO: add repo to /etc/pacman.conf
+# TODO:
+# add repo to /etc/pacman.conf
+# create and su into 'jsalcido'
+# install yay
+# install jsalix package(s)
+# install user configs (possibly with a dotfile manager?)
+# enable applicable services
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si
+makepkg -si --needed --noconfirm
 cd ..
 rm -rf yay
 
