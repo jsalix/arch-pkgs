5db36427ee7be1789cdd3ef4998981e0983e6357
Josiah Salcido <commander225@gmail.com>
2019-04-17 12:28:28 -0700

    update repo and install script

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-# Setup yay and install jsalix-arch
+# Setup yay and install jsalix-base
+
+# TODO: add repo to /etc/pacman.conf
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-
 cd ..
 rm -rf yay
-cd jsalix-arch
-makepkg -si
+
+yay -S jsalix-base
 
 echo "Done!"
